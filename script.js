d2f8ae3098c68a8ba27c596523e83ff765e34b5f
Aniket Gauda <122367767+AniketGauda@users.noreply.github.com>
2024-05-26 14:30:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-function insert_Row() {
-    //Write your code here
-  let tbl = document.getElementById("sampleTable");
-
-    tbl.innerHTML = `<tr>
-				<td>New Cell1</td> 
-	            <td>New Cell2</td>
-			</tr>` + tbl.innerHTML;
-  
+function insert_Row(){
+    let tbl = document.getElementById("sampleTable");
+    tbl.innerHTML = `
+        <tr>
+            <td>New Cell1</td>
+            <td>New Cell2</td>
+        </tr>
+    `+tbl.innerHTML;
 }
